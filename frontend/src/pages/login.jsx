c8654be975518aeae3006667d471802294d97bb9
Shredhaya <shredhayagoel@gmail.com>
2026-09-22 23:56:20 +0530

    handled login added api

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
     e.preventDefault();
     try {
       /*
-      const response = await fetch(url, {
+      const response = await fetch("http://localhost:5000/api/login", {
   method: "POST",
   headers: {
     "Content-Type": "application/json"
